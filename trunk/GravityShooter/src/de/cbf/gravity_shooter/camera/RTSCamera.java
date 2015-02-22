a0eb0a6b3008b24545f49205103dcb57cc425c19
alti@gmx.de <alti@gmx.de@1cd0f577-f6a1-8d1d-523e-25171bf15a10>
2015-02-22 22:27:09 +0000

    - changed camera to be moved via middle mouse button press and moving

--- trunk/GravityShooter/src/de/cbf/gravity_shooter/camera/RTSCamera.java
+++ trunk/GravityShooter/src/de/cbf/gravity_shooter/camera/RTSCamera.java
@@ -3,16 +3,16 @@
  */
 package de.cbf.gravity_shooter.camera;
 
-import java.util.logging.Logger;
-
 import com.jme3.collision.MotionAllowedListener;
 import com.jme3.input.FlyByCamera;
 import com.jme3.input.InputManager;
 import com.jme3.input.KeyInput;
 import com.jme3.input.MouseInput;
+import com.jme3.input.controls.ActionListener;
 import com.jme3.input.controls.AnalogListener;
 import com.jme3.input.controls.KeyTrigger;
 import com.jme3.input.controls.MouseAxisTrigger;
+import com.jme3.input.controls.MouseButtonTrigger;
 import com.jme3.math.FastMath;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.Camera;
@@ -21,7 +21,7 @@ import com.jme3.renderer.Camera;
  * @author Alti
  *
  */
-public class RTSCamera implements AnalogListener{
+public class RTSCamera implements AnalogListener,ActionListener{
 //	private static final Logger LOGGER = Logger.getLogger(RTSCamera.class.getName());
 	
 	public static final String CAMERA_ZOOM_IN = "CameraZoomIn";
@@ -32,21 +32,25 @@ public class RTSCamera implements AnalogListener{
     public static final String CAMERA_SOUTH = "CameraSouth";
     public static final String CAMERA_RISE = "CameraRise";
     public static final String CAMERA_LOWER = "CameraLower";
+    
+    public static final String MIDDLE_MOUSE = "MiddleMouse";
 	
     public static final String[] CAMERA_MAPPINGS = new String[]{
     	CAMERA_ZOOM_IN,CAMERA_ZOOM_OUT,
     	CAMERA_WEST,CAMERA_EAST,CAMERA_NORTH,CAMERA_SOUTH,
-    	CAMERA_RISE,CAMERA_LOWER
+    	CAMERA_RISE,CAMERA_LOWER,
+    	MIDDLE_MOUSE
     };
     
     
     protected Camera cam;
     protected Vector3f initialUpVec;
-    protected float moveSpeed = 3f;    
+    protected float moveSpeed = 5f;    
     protected float zoomSpeed = 5f;
     protected boolean enabled = true;
     protected MotionAllowedListener motionAllowed = null;
     protected InputManager inputManager;
+	private boolean middleMousePressed;
     
     public RTSCamera(Camera cam) {
 		super();
@@ -57,13 +61,15 @@ public class RTSCamera implements AnalogListener{
 	public void registerWithInput(InputManager inputManager) {
 		this.inputManager = inputManager;
 		
+		inputManager.addMapping(MIDDLE_MOUSE, new MouseButtonTrigger(MouseInput.BUTTON_MIDDLE));
+		
 		inputManager.addMapping(CAMERA_ZOOM_IN, new MouseAxisTrigger(MouseInput.AXIS_WHEEL, false));
         inputManager.addMapping(CAMERA_ZOOM_OUT, new MouseAxisTrigger(MouseInput.AXIS_WHEEL, true));
 	    
-	    inputManager.addMapping(CAMERA_WEST, new KeyTrigger(KeyInput.KEY_A));
-        inputManager.addMapping(CAMERA_EAST, new KeyTrigger(KeyInput.KEY_D));
-        inputManager.addMapping(CAMERA_NORTH, new KeyTrigger(KeyInput.KEY_W));
-        inputManager.addMapping(CAMERA_SOUTH, new KeyTrigger(KeyInput.KEY_S));
+	    inputManager.addMapping(CAMERA_WEST, new MouseAxisTrigger(MouseInput.AXIS_X,false)); 
+        inputManager.addMapping(CAMERA_EAST, new MouseAxisTrigger(MouseInput.AXIS_X,true));
+        inputManager.addMapping(CAMERA_NORTH, new MouseAxisTrigger(MouseInput.AXIS_Y,true));
+        inputManager.addMapping(CAMERA_SOUTH, new MouseAxisTrigger(MouseInput.AXIS_Y,false));
         inputManager.addMapping(CAMERA_RISE, new KeyTrigger(KeyInput.KEY_Q));
         inputManager.addMapping(CAMERA_LOWER, new KeyTrigger(KeyInput.KEY_Y));
         
@@ -120,31 +126,31 @@ public class RTSCamera implements AnalogListener{
     
 
     protected void moveCamera(float value,EAxis axis){
-        Vector3f vel = new Vector3f();
-        Vector3f pos = cam.getLocation().clone();
+        Vector3f velocity = new Vector3f();
+        Vector3f position = cam.getLocation().clone();
 
         switch (axis) {
 		case NORTH_SOUTH:
-			cam.getUp(vel);
+			cam.getUp(velocity);
 			break;
 		case WEST_EAST:
-			cam.getLeft(vel);
+			cam.getLeft(velocity);
 			break;
 		case UP_DOWN:
-			cam.getDirection(vel);
+			cam.getDirection(velocity);
 			break;			
 		default:
 			break;
 		}
 
-        vel.multLocal(value * moveSpeed);
+        velocity.multLocal(value * moveSpeed);
 
         if (motionAllowed != null)
-            motionAllowed.checkMotionAllowed(pos, vel);
+            motionAllowed.checkMotionAllowed(position, velocity);
         else
-            pos.addLocal(vel);
+            position.addLocal(velocity); //change position by adding velocity vector
 
-        cam.setLocation(pos);
+        cam.setLocation(position);
     }
 
 
@@ -154,24 +160,30 @@ public class RTSCamera implements AnalogListener{
 //		LOGGER.log(Level.INFO, "AnalogInput! Name:"+name);
 		if (!enabled)
             return;       
-        if (name.equals(CAMERA_NORTH)){
+        if (middleMousePressed && CAMERA_NORTH.equals(name)){
         	moveCamera(value, EAxis.NORTH_SOUTH);           
-        }else if (name.equals(CAMERA_SOUTH)){            
+        }else if (middleMousePressed && CAMERA_SOUTH.equals(name)){            
         	moveCamera(-value, EAxis.NORTH_SOUTH);
-        }else if (name.equals(CAMERA_WEST)){
+        }else if (middleMousePressed && CAMERA_WEST.equals(name)){
             moveCamera(value, EAxis.WEST_EAST);
-        }else if (name.equals(CAMERA_EAST)){
+        }else if (middleMousePressed && CAMERA_EAST.equals(name)){
             moveCamera(-value,EAxis.WEST_EAST);
-        }else if (name.equals(CAMERA_RISE)){
+        }else if (middleMousePressed && CAMERA_RISE.equals(name)){
         	moveCamera(value,EAxis.UP_DOWN);
-        }else if (name.equals(CAMERA_LOWER)){        	
+        }else if (middleMousePressed && CAMERA_LOWER.equals(name)){        	
         	moveCamera(-value,EAxis.UP_DOWN);
-        }else if (name.equals(CAMERA_ZOOM_OUT)){
+        }else if (CAMERA_ZOOM_OUT.equals(name)){
             zoomCamera(value);
-        }else if (name.equals(CAMERA_ZOOM_IN)){
+        }else if (CAMERA_ZOOM_IN.equals(name)){
             zoomCamera(-value);
         }
 	}
+	
+	public void onAction(String name, boolean isPressed, float tpf){
+		if(MIDDLE_MOUSE.equals(name)){
+			middleMousePressed = isPressed;
+		}
+	};
 
     public void setMotionAllowedListener(MotionAllowedListener listener){
         this.motionAllowed = listener;
